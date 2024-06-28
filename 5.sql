bcc982f85021724b4288eb3fcf85b29815e08d74
Vled5445 <101985341+Vled5445@users.noreply.github.com>
2024-06-28 18:52:47 +0300

    Update 5.sql

--- 5.sql
+++ 5.sql
@@ -1,17 +1,21 @@
+
 create table stage.course
 (
     name String,
     duration_day UInt16,
-    lanuage LowCardinality(String)
+    lanuage LowCardinality(String),
+    description String,
+    author_id  UInt32
 )
 engine = MergeTree()
 order by name
 
-  
 create table direct_log.course
 (
-    name String,
+        name String,
     duration_day UInt16,
-    lanuage LowCardinality(String)
+    lanuage LowCardinality(String),
+    description String,
+    author_id  UInt32
 )
-engine = Buffer(stage,course,16, 10, 100, 10000, 1000000, 10000000, 100000000)
+engine = Buffer(stage,course,16, 10, 100, 10000, 1000000, 10000000, 100000000);
