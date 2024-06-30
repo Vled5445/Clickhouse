84aedd895b63c9a7a26c18e1e1486e6b0c492837
Vled5445 <101985341+Vled5445@users.noreply.github.com>
2024-06-30 10:51:26 +0300

    Update 4.sql

--- 4.sql
+++ 4.sql
@@ -1,9 +1,22 @@
+-- Создаем роль stg_rw для чтения и вставки в stg слой
+
 create role stg_rw;
+-- Выдаем данной роли права на вставку
+
 grant insert on stage.* to stg_rw;
+
+-- Выдаем права на создание таблиц
+
 grant create table on stage.* to stg_rw;
 
+-- Создаем роль stg_readonly
+
 create role stg_readonly;
+
+-- Выдаем права на чтение
+
 grant select on stage.* to stg_readonly;
+
 
 create user readonl1;
 grant stg_readonly to readonl1;
